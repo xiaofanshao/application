a6aebfa4c347045ed4077dd90e4e2c0ed78df20a
xiaofanshao <404457041@qq.com>
2018-11-26 16:21:04 +0800

    提交文件

---
+++ index/controller/Login.php
@@ -0,0 +1,87 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+
+class Login extends Controller{
+	public function index(){
+      return $this->fetch();
+    }
+   public function register(){
+     return $this->fetch();
+   }
+  public function doRegist(){
+   $param = input('post.');
+    	if(empty($param['user_name'])){
+    		
+    		$this->error('用户名不能为空');
+    	}
+    	
+    	if(empty($param['user_pwd'])){
+    		
+    		$this->error('密码不能为空');
+    	}
+    	
+    	// 验证用户名
+    //此处变成注册
+   	 	$data = ['user_name' => $param['user_name'], 'user_pwd' => md5($param['user_pwd'])];
+		db('users')->insert($data);
+    
+    	$has = db('users')->where('user_name', $param['user_name'])->find();
+    	if(empty($has)){
+    		
+    		$this->error('用户名密码错误');
+    	}
+    	
+    	// 验证密码
+    	if($has['user_pwd'] != md5($param['user_pwd'])){
+    		
+    		$this->error('用户名密码错误');
+    	}
+    	
+    	// 记录用户登录信息
+    
+    	cookie('user_id', $has['id'], 3600);  // 一个小时有效期
+    	cookie('user_name', $has['user_name'], 3600);
+    	
+    	$this->redirect(url('index/index'));
+  }
+   public function doLogin()
+    {
+    	$param = input('post.');
+    	if(empty($param['user_name'])){
+    		
+    		$this->error('用户名不能为空');
+    	}
+    	
+    	if(empty($param['user_pwd'])){
+    		
+    		$this->error('密码不能为空');
+    	}
+    	
+    	// 验证用户名
+    	$has = db('users')->where('user_name', $param['user_name'])->find();
+    	if(empty($has)){
+    		
+    		$this->error('用户名密码错误');
+    	}
+    	
+    	// 验证密码
+    	if($has['user_pwd'] != md5($param['user_pwd'])){
+    		
+    		$this->error('用户名密码错误');
+    	}
+    	
+    	// 记录用户登录信息
+    	cookie('user_id', $has['id'], 3600);  // 一个小时有效期
+    	cookie('user_name', $has['user_name'], 3600);
+    	
+    	$this->redirect(url('index/index'));
+    }
+  public function loginout(){
+    cookie('user_id',null);
+    cookie('user_name',null);
+    
+    $this->redirect(url('login/index'));
+  }
+}
---
+++ index/controller/Weather.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\index\controller;
+
+class Weather
+{
+    public function index()
+    {
+       echo "您好，今天天气不错";
+    }
+}
